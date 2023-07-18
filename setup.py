d3bdf9c8fea1a2f1c53f2527cc3d28930f583aad
Jerick Gutierrez <jerickgutierrez1@gmail.com>
2023-07-17 17:04:15 -0700

    added documentation

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup
 setup(
     name='flask_protobuf',
     license='MIT',
-    version='0.1.1',
+    version='0.1.3',
     description='Flask-Protobuf is a Python package that provides integration between Flask and Protocol Buffers (protobuf). It allows you to easily handle incoming protobuf messages in your Flask application.',
     author='Jerick Gutierrez',
     packages=['flask_protobuf'],
     url='https://github.com/Mackhintoshi/Flask-Protobuf',
     keywords=['flask', 'protobuf', 'google', 'protocol buffers'],
+    readme='README.md',
     install_requires=[
         'Flask>=2.3.2',
         'protobuf>=4.23.4'
